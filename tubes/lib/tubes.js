fb234625a3a1b36c1a571c003d9bce738df83f1b
Sandro Amato <sandro@mylinbox.mydomai.my>
2016-05-01 11:28:07 +0200

    Not shifting menu and bordered images

--- tubes/lib/tubes.js
+++ tubes/lib/tubes.js
@@ -7,15 +7,17 @@ function w3_sidenav_close() {
 }
 */
 function w3_sidenav_open() {
-  document.getElementById("main").style.marginLeft = "25%";
-  document.getElementsByClassName("w3-sidenav")[0].style.width = "25%";
+  //document.getElementById("main").style.marginLeft = "25%";
+  //document.getElementById("main").style.marginLeft = "0%";
+  //document.getElementsByClassName("w3-sidenav")[0].style.width = "25%";
+  document.getElementsByClassName("w3-sidenav")[0].style.width = "256px";
   document.getElementsByClassName("w3-sidenav")[0].style.display = "block";
-  document.getElementsByClassName("w3-opennav")[0].style.display = 'none';
+  //document.getElementsByClassName("w3-opennav")[0].style.display = 'none';
 }
 function w3_sidenav_close() {
-  document.getElementById("main").style.marginLeft = "0%";
+  //document.getElementById("main").style.marginLeft = "0%";
   document.getElementsByClassName("w3-sidenav")[0].style.display = "none";
-  document.getElementsByClassName("w3-opennav")[0].style.display = "inline-block";
+  //document.getElementsByClassName("w3-opennav")[0].style.display = "inline-block";
 }
 function my_modal_open(aUrl) {
   $('#tubes-info-content').load(aUrl);
